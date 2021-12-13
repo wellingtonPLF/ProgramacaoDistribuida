912d4edf5625052bbbeb9bf636646a8d0db5d84c
WellingtonPLF <42122392+wellingtonPLF@users.noreply.github.com>
2021-12-13 14:13:30 -0300

    Update Servidor.java

--- atv_7/RPCMenssager/src/main/java/pratica/pd/main/Servidor.java
+++ atv_7/RPCMenssager/src/main/java/pratica/pd/main/Servidor.java
@@ -9,12 +9,6 @@ import com.rabbitmq.client.DeliverCallback;
 public class Servidor {
 	 private static final String RPC_QUEUE_NAME = "MinhaFila";
 
-	    private static int fib(int n) {
-	        if (n == 0) return 0;
-	        if (n == 1) return 1;
-	        return fib(n - 1) + fib(n - 2);
-	    }
-
 	    public static void main(String[] argv) throws Exception {
 	        ConnectionFactory factory = new ConnectionFactory();
 	        factory.setHost("localhost");
